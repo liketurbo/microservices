ee17cb7920560383588f60c6144d2b2e953ee2a6
Ramzan Chasygov <theliketurbo@gmail.com>
2019-03-21 18:29:35 +0300

    dockerfile for web package

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+
+import Fib from './Fib';
+import logo from './logo.svg';
+import OtherPage from './OtherPage';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <Link to="/">Home</Link>
+            <Link to="/other-page">Other page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route exact path="/other-page" component={OtherPage} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
