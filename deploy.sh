0eb51a1b13ac97ba8da4e48313cb7592ab9643ba
Ramzan Chasygov <theliketurbo@gmail.com>
2019-05-11 10:13:19 +0300

    fixed bug with incorrect component name

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ docker push liketurbo/microservices-worker:$GIT_SHA
 
 kubectl apply -f ./packages/k8s
 
-kubectl set image deployments/web-deployment client=liketurbo/microservices-web:$GIT_SHA
+kubectl set image deployments/web-deployment web=liketurbo/microservices-web:$GIT_SHA
 kubectl set image deployments/server-deployment server=liketurbo/microservices-server:$GIT_SHA
 kubectl set image deployments/worker-deployment worker=liketurbo/microservices-worker:$GIT_SHA
